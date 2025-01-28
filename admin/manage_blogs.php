7946cd5bf32d53ca070dddd716f1941651b82259
SBhawana <bhawana6851@gmail.com>
2025-01-29 00:11:00 +0530

    Manage blog content setup

--- admin/manage_blogs.php
+++ admin/manage_blogs.php
@@ -1,13 +1,15 @@
 <?php
 require_once './includes/header.php';
 ?>
+<link rel="stylesheet" href="https://cdn.ckeditor.com/ckeditor5/44.1.0/ckeditor5.css" />
+
 
 <!-- HEADER OF THE PAGE -->
 <div class="app-content-header">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-sm-6">git branch -M main
-                
+            <div class="col-sm-6">
+
                 <h3 class="mb-0">Manage Blogs</h3>
             </div>
             <div class="col-sm-6">
@@ -21,6 +23,38 @@ require_once './includes/header.php';
     </div>
 </div>
 
+<div id="editor">
+    <p>Hello from CKEditor 5!</p>
+</div>
+
+
 <?php
 require_once './includes/footer.php';
 ?>
+
+
+<script src="https://cdn.ckeditor.com/ckeditor5/44.1.0/ckeditor5.umd.js"></script>
+<script>
+    const {
+        ClassicEditor,
+        Essentials,
+        Bold,
+        Italic,
+        Font,
+        Paragraph
+    } = CKEDITOR;
+
+    ClassicEditor
+        .create(document.querySelector('#editor'), {
+            licenseKey: '<YOUR_LICENSE_KEY>',
+            plugins: [Essentials, Bold, Italic, Font, Paragraph],
+            toolbar: [
+                'undo', 'redo', '|', 'bold', 'italic', '|',
+                'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor'
+            ]
+        })
+        .then( /* ... */ )
+        .catch( /* ... */ );
+
+        
+</script>
