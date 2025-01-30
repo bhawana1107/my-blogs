fc7f50a2b2e54c7cb5b7de17ce3046f33d750e78
SBhawana <bhawana6851@gmail.com>
2025-01-30 23:20:59 +0530

    changes in blog section

--- admin/manage_blogs.php
+++ admin/manage_blogs.php
@@ -1,5 +1,8 @@
 <?php
 require_once './includes/header.php';
+@include '../private.php';
+$ck_editor_key = defined('CK_EDITOR_KEY') ? CK_EDITOR_KEY : 'nhi h';
+
 ?>
 <link rel="stylesheet" href="https://cdn.ckeditor.com/ckeditor5/44.1.0/ckeditor5.css" />
 
@@ -23,10 +26,43 @@ require_once './includes/header.php';
     </div>
 </div>
 
-<div id="editor">
-    <p>Hello from CKEditor 5!</p>
-</div>
+<!-- FORM OF CONTENT  -->
+<div class="container-fluid">
+    <div class="row g-4">
+        <div class="col-md-12">
+            <div class="card card-primary card-outline mb-4">
+                <div class="card-header">
+                    <div class="card-title">Add Blogs</div>
+                </div>
+                <form method="post" action="">
+                    <div class="card-body">
+                        <div class="mb-3">
+                            <label for="blogName" class="form-label">Blog Name</label>
+                            <input type="text" class="form-control" id="blogName">
+                        </div>
+                        <div class="mb-3">
+                            <label for="blogCategory" class="form-label">Blog Category</label>
+                            <select class="form-select" id="blogCategory">
+                                <option selected="" disabled="" value="">Choose...</option>
+                                <option>...</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="blogContent" class="form-label">Blog Content</label>
+                            <div id="editor">
+                                <p>Hello from CKEditor 5!</p>
+                            </div>
+                        </div>
 
+                    </div>
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php
 require_once './includes/footer.php';
@@ -41,20 +77,55 @@ require_once './includes/footer.php';
         Bold,
         Italic,
         Font,
-        Paragraph
+        Paragraph,
+        Heading,
+        BlockQuote,
+        Link,
+        List,
+        Alignment,
+        Image,
+        ImageUpload,
+        Code,
+        CodeBlock,
+        Strikethrough,
+        Subscript,
+        Superscript,
+        TodoList,
+
     } = CKEDITOR;
 
     ClassicEditor
         .create(document.querySelector('#editor'), {
-            licenseKey: '<YOUR_LICENSE_KEY>',
-            plugins: [Essentials, Bold, Italic, Font, Paragraph],
-            toolbar: [
-                'undo', 'redo', '|', 'bold', 'italic', '|',
-                'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor'
-            ]
-        })
-        .then( /* ... */ )
-        .catch( /* ... */ );
+            licenseKey: '<?= $ck_editor_key ?>',
+            plugins: [
+                Essentials, Bold, Italic, Font, Paragraph, Heading, BlockQuote,
+                Link, List, Alignment, Image, ImageUpload, Code, CodeBlock,
+                Strikethrough, Subscript, Superscript, TodoList
+            ],
 
-        
+            toolbar: {
+                items: [
+                    'undo', 'redo',
+                    '|',
+                    'heading',
+                    '|',
+                    'fontfamily', 'fontsize', 'fontColor', 'fontBackgroundColor',
+                    '|',
+                    'bold', 'italic', 'strikethrough', 'subscript', 'superscript', 'code',
+                    '|',
+                    'link', 'uploadImage', 'blockQuote', 'codeBlock',
+                    '|',
+                    'alignment',
+                    '|',
+                    'bulletedList', 'numberedList', 'todoList', 'outdent', 'indent'
+                ],
+                shouldNotGroupWhenFull: true
+            },
+        })
+        .then(editor => {
+            console.log("Editor loaded successfully", editor);
+        })
+        .catch(error => {
+            console.error("Editor error:", error);
+        });
 </script>
